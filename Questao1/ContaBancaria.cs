d206ddb24f01b38387cb54d24bac2a256cf69ad2
suhettgabriel <suhettgabriel1983@gmail.com>
2024-07-22 10:25:50 -0300

    Adicionar arquivos de projeto.

---
+++ Questao1/ContaBancaria.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+
+namespace Questao1
+{
+    class ContaBancaria
+    {
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        private const double TaxaDeSaque = 3.50;
+
+        public ContaBancaria(int numero, string titular, double depositoInicial)
+        {
+            Numero = numero;
+            Titular = titular;
+            Deposito(depositoInicial);
+        }
+
+        public ContaBancaria(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+            Saldo = 0.0;
+        }
+
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        public void Saque(double quantia)
+        {
+            Saldo -= quantia + TaxaDeSaque;
+        }
+
+        public override string ToString()
+        {
+            return $"Conta {Numero}, Titular: {Titular}, Saldo: $ {Saldo.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
