344b3db7b2ce366990def619a715d48a62d85c08
suhettgabriel <suhettgabriel1983@gmail.com>
2024-07-22 10:41:06 -0300

    resolução_questao2

---
+++ Questao2/Match.cs
@@ -0,0 +1,10 @@
+﻿namespace Questao2
+{
+    public class Match
+    {
+        public string team1 { get; set; }
+        public string team2 { get; set; }
+        public string team1goals { get; set; }
+        public string team2goals { get; set; }
+    }
+}
--- Questao2/Program.cs
+++ Questao2/Program.cs
@@ -1,30 +1,86 @@
 ﻿using Newtonsoft.Json;
+using Questao2;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 public class Program
 {
-    public static void Main()
+    public static async Task Main(string[] args)
     {
         string teamName = "Paris Saint-Germain";
         int year = 2013;
-        int totalGoals = getTotalScoredGoals(teamName, year);
-
-        Console.WriteLine("Team "+ teamName +" scored "+ totalGoals.ToString() + " goals in "+ year);
-
-        teamName = "Chelsea";
-        year = 2014;
-        totalGoals = getTotalScoredGoals(teamName, year);
+        int totalGoals = await getTotalScoredGoals(teamName, year);
 
         Console.WriteLine("Team " + teamName + " scored " + totalGoals.ToString() + " goals in " + year);
 
-        // Output expected:
-        // Team Paris Saint - Germain scored 109 goals in 2013
-        // Team Chelsea scored 92 goals in 2014
+        teamName = "Chelsea";
+        year = 2014;
+        totalGoals = await getTotalScoredGoals(teamName, year);
+
+        Console.WriteLine("Team " + teamName + " scored " + totalGoals.ToString() + " goals in " + year);
     }
 
-    public static int getTotalScoredGoals(string team, int year)
+    public static async Task<int> getTotalScoredGoals(string team, int year)
     {
-        
-        return 0;
-    }
+        int totalGoals = 0;
+        int currentPage = 1;
+        bool hasMorePages = true;
 
+        using (HttpClient client = new HttpClient())
+        {
+            while (hasMorePages)
+            {
+                string url = $"https://jsonmock.hackerrank.com/api/football_matches?year={year}&team1={team}&page={currentPage}";
+                HttpResponseMessage response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonResponse = await response.Content.ReadAsStringAsync();
+                    var result = JsonConvert.DeserializeObject<ApiResponse>(jsonResponse);
+
+                    foreach (var match in result.data)
+                    {
+                        totalGoals += int.Parse(match.team1goals);
+                    }
+
+                    hasMorePages = currentPage < result.total_pages;
+                    currentPage++;
+                }
+                else
+                {
+                    hasMorePages = false;
+                }
+            }
+
+            currentPage = 1;
+            hasMorePages = true;
+
+            while (hasMorePages)
+            {
+                string url = $"https://jsonmock.hackerrank.com/api/football_matches?year={year}&team2={team}&page={currentPage}";
+                HttpResponseMessage response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonResponse = await response.Content.ReadAsStringAsync();
+                    var result = JsonConvert.DeserializeObject<ApiResponse>(jsonResponse);
+
+                    foreach (var match in result.data)
+                    {
+                        totalGoals += int.Parse(match.team2goals);
+                    }
+
+                    hasMorePages = currentPage < result.total_pages;
+                    currentPage++;
+                }
+                else
+                {
+                    hasMorePages = false;
+                }
+            }
+        }
+
+        return totalGoals;
+    }
 }
+
+
